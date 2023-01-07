39344cfdf8fc3e9bc4c0368ebcd5320e00377c3e
Pavithran2 <pavithrank02@gmail.com>
2023-01-07 14:13:41 +0530

    today

--- App.js
+++ App.js
@@ -2,16 +2,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 const heading = (
-  <h1>Namaste</h1>
+  <h1>Food Villa</h1>
   )
 const HeaderComponent = () => {
   return (
-    <div>
+    <div className="header">
       {heading}
-      <h1>Namaste React Functional Components</h1>
-      <h2>Namaste h2</h2>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   )
 }
+
+const RestaurantCard = () => {
+  return (
+     <div className="card">
+
+     </div>
+  )
+}
+
+const Body = () => {
+
+}
+
+const Footer = () => {
+
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeaderComponent />);
