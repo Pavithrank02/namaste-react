6db16a72f2018c64ead92bf941b92de506c43270
Pavithran2 <pavithrank02@gmail.com>
2023-01-19 23:26:15 +0530

    contact and about us added

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
+import about from "../assets/image/image 2.jpg";
 const About = () => {
   return(
-    <div>
+    <div className="about-us">
       <h1>About Us Page</h1>
-      <p>gbskdbksdgbd</p>
+      <img className="img" alt="logo" src={about} />
+      <p>S.Abdul Samad started selling homemade chicken biryani & chicken 65 from a small road side stall in broadway, Chennai. His hard work, perseverance and passion helped to stabilise his business increased in volume steadily over the years. What’s the secret to his taste recipes? It is his mother’s secret receipe which he cherishes to this day. This business was later conducted under the name and style of SS Hyderabad Biryani Pvt Ltd - a brand name which is very popular amongst all biryani lovers. The first eatery was started on a small scale in 1998 in Perambur with a seating capacity of 8. This eatery was so popular and led the way for starting a series of branches all over city.</p>
     </div>
   )
 
