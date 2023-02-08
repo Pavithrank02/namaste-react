5c66e6101a9721d03634c367bd5424ad0a1eefa9
Pavithran2 <pavithrank02@gmail.com>
2023-02-08 11:07:52 +0530

    adding reducers

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -9,8 +9,14 @@ const cartSlice = createSlice({
     addItem: (state, action) => {
         state.items.push(action.payload)
     },
+    removeItem: (state, action) => {
+      state.items.pop()
+  },
     clearCart: (state) => {
       state.items = []
     }
   }
 })
+
+export const {addItem, removeItem, clearCart} = cartSlice.actions;
+export default cartSlice.reducer;
